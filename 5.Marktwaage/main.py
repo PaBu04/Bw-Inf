5c2c208dc0c0b4973b7159a8e1dada49f1a5be0b
PaBu04 <88597864+PaBu04@users.noreply.github.com>
2021-11-15 21:34:38 +0100

    Update main.py

--- 5.Marktwaage/main.py
+++ 5.Marktwaage/main.py
@@ -28,6 +28,7 @@ for r in range(numberWeights):
     amounts.append([r for r in range(amount * -1, amount + 1)])
     calcs *= amount * 2 + 1
     
+print(weightsList)
 print("Needed Calculations: " + str(int((calcs - 1) / 2)))
 print("Needed Time: " + str(int((calcs - 1) / 2) / 30000) + "s")
 
@@ -45,18 +46,18 @@ possibleWeights.sort(key = lambda w: (w[1], sum(abs(x) for x in w[0])), reverse 
 #Reverse the sign if the sum of the weight is negative
 possibleWeights = [([-w if (weight[1] < 0) else w for w in weight[0]], -weight[1]) for weight in possibleWeights if(weight[1] < 11000 and weight[1] > -11000)]
 
+last = possibleWeights[-1]
 possibleWeights = [(possibleWeights[r]) for r in range(len(possibleWeights) - 1) if(possibleWeights[r + 1][1] != possibleWeights[r][1])]
-
-print(time.time() - start)
+possibleWeights.append(last)
 
 row = [w[1] for w in possibleWeights]
-
-print(time.time() - start)
 
 #sort the values
 for r in range(10, 10010, 10):
     
     mini = min(possibleWeights, key = lambda x:abs(x[1] - r))
-    print(str(r) + "g -> " + str(mini[1]) + "g " + str(mini[0]))
+    weightsRight = [(str(mini[0][r]) + "*" + str(weightsList[r]) + "g") for r in range(len(weightsList)) if(mini[0][r] > 0)]
+    weightsLeft = [(str(abs(mini[0][r])) + "*" + str(weightsList[r]) + "g") for r in range(len(weightsList)) if(mini[0][r] < 0)]
+    print(str(r) + "g -> " + str(mini[1]) + "g\nRight: " + str(weightsRight)[1:-1].replace("'", "") + "\nLeft: " + str(weightsLeft)[1:-1].replace("'", ""))
     
 print(time.time() - start)
