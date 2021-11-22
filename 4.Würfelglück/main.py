c783e239511f53c67304df24197b4f8b71db8f51
Lorenz Wildberg <lorenz@wild-fisch.de>
2021-11-22 21:16:14 +0000

    fix

--- 4.Würfelglück/main.py
+++ 4.Würfelglück/main.py
@@ -114,10 +114,12 @@ class Spieler:
                 return
         return
 
-    def get_weiteste_ziehbare_figur(self, felder: int) -> Figur:
+    def get_weiteste_ziehbare_figur(self, felder: int, nicht_im_ziel: bool = True) -> Figur:
         weiteste_figur: Figur = None
         for figur in self.figuren:
             if (weiteste_figur == None or figur.feld > weiteste_figur.feld) and self.darf_vorruecken(figur, felder):
+                if nicht_im_ziel and figur.im_ziel():
+                    continue
                 weiteste_figur = figur
         return weiteste_figur
 
@@ -145,8 +147,10 @@ class Spielfeld:
             # ziehe ganz normal
             else:
                 figur: Figur = self.am_zug.get_weiteste_ziehbare_figur(augenzahl)
-                if figur != None:
-                    self.vorruecken_mit_schlagen(figur, augenzahl)
+                if figur == None:
+                    figur = self.am_zug.get_weiteste_ziehbare_figur(augenzahl, False)
+                self.vorruecken_mit_schlagen(figur, augenzahl)
+
 
             if augenzahl != 6:
                 break
